7173a49163e3fac2ac47353b8f329764d0549ee4
Sebastián T. <98891697+SebastianT454@users.noreply.github.com>
2026-05-25 22:01:13 -0500

    cypress.config.js modificado

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "sd86db",
   e2e: {
-    supportFile: 'cypress/support/e2e.js',
-    baseUrl: 'http://localhost:8000',
-    },
+    supportFile: "cypress/support/e2e.js",
+    baseUrl: "http://localhost:8000",
   },
-);
+});
